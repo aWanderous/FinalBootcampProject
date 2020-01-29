98fcd9abcdaed736f53269e3679889fd02a76191
Alex Wan <a_wanderous@hotmail.com>
2020-01-30 04:37:16 +1100

    added sign in buttons

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Jumbotron from "../components/Jumbotron";
 import {Container } from "../components/Grid";
+import { FormBtn } from "../components/Form";
 import Footer from "../components/Footer";
 
 class Login extends Component {
@@ -9,8 +10,10 @@ class Login extends Component {
 		return (
 			<Container fluid>
 				<Jumbotron>
-					login
+					Sign in
 				</Jumbotron>
+				<FormBtn className="login btn-lg">Login</FormBtn>
+				<FormBtn className="btn-lg">Register</FormBtn>
 				<Footer/>
 			</Container>
 		);
