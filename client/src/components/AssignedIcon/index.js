af2b4af97fc4b35e9351c395e7f8f37c4675db90
Alex Wan <a_wanderous@hotmail.com>
2020-01-30 02:18:26 +1100

    change links to assign and dollar icons

--- client/src/components/AssignedIcon/index.js
+++ client/src/components/AssignedIcon/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "./style.css";
 import Assign from "./blue-person.png"
+import { Link } from "react-router-dom";
 
 function AssignIcon() {
   return (
-    <img className="assigned" src={Assign} alt= "Assigned" />
+    <Link to={"/Helper/"}>
+      <img className="assigned" src={Assign} alt= "Assigned" />
+    </Link>
   );
 }
 
--- client/src/components/DollarIcon/index.js
+++ client/src/components/DollarIcon/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import "./style.css";
 import Dollar from "./blue-dollar.png"
+import { Link } from "react-router-dom";
 
 function DollarIcon() {
   return (
-    <img className="priced" src={Dollar} alt= "Priced" />
+    <Link to={"/Costs/"}>
+      <img className="priced" src={Dollar} alt= "Priced"/>
+    </Link>
   );
 }
 
